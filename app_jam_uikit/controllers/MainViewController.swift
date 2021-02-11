25fa17e8628f83c56ee6276de1a47075fe22f43a
Markim Shaw <ms79723@gmail.com>
2021-02-11 03:57:16 -0600

    ✅ Add UINavigationControllers
    - Override light/dark theme for status bar
    - Place dummy view controllers in navigation controller

--- app_jam_uikit/controllers/MainViewController.swift
+++ app_jam_uikit/controllers/MainViewController.swift
@@ -8,22 +8,51 @@
 import Foundation
 import UIKit
 
-class MainViewController: UITabBarController {
+class NavigationController : UINavigationController {
   
-  lazy var viewController: ViewController = ViewController()
-  lazy var loginController: LoginViewController = LoginViewController()
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
+  override var preferredStatusBarStyle : UIStatusBarStyle {
     
-    self.view.backgroundColor = .systemBackground
+    if let topVC = viewControllers.last {
+      //return the status property of each VC, look at step 2
+      return topVC.preferredStatusBarStyle
+    }
     
-    viewController.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "photo"), selectedImage: UIImage(systemName: "photo.fill"))
-    loginController.tabBarItem = UITabBarItem(title: "Login", image: UIImage(systemName: "livephoto"), selectedImage: UIImage(systemName: "livephoto.slash"))
-    
-    let controllerList = [viewController, loginController]
-    
-    self.viewControllers = controllerList
-    
+    return .default
   }
 }
+  
+  class MainViewController: UITabBarController {
+    
+    lazy var viewController: ViewController = ViewController()
+    lazy var loginController: LoginViewController = LoginViewController()
+    
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      
+      self.view.backgroundColor = .systemBackground
+      
+      viewController.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "photo"), selectedImage: UIImage(systemName: "photo.fill"))
+      
+      
+      loginController.tabBarItem = UITabBarItem(title: "Login", image: UIImage(systemName: "livephoto"), selectedImage: UIImage(systemName: "livephoto.slash"))
+      
+      let controllerList = [viewController, loginController]
+      
+      self.viewControllers = controllerList.map { NavigationController(rootViewController: $0) }
+      
+      viewController.navigationController?.navigationBar.prefersLargeTitles = true
+      viewController.navigationController?.navigationBar.standardAppearance = viewControllerNavigationAppearance()
+      viewController.navigationController?.navigationBar.scrollEdgeAppearance = viewControllerNavigationAppearance()
+      
+    }
+    
+    fileprivate func viewControllerNavigationAppearance() -> UINavigationBarAppearance {
+      let coloredAppearance = UINavigationBarAppearance()
+      coloredAppearance.configureWithTransparentBackground()
+      coloredAppearance.backgroundColor = .systemPink
+      coloredAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+      coloredAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.label]
+      
+      return coloredAppearance
+    }
+  }

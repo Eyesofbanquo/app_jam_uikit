f38809060ea8765abfd4419889654c3cf80850a8
Markim Shaw <ms79723@gmail.com>
2021-02-19 01:57:47 -0600

    ⚡️JSONDecoder factory pattern implementation

--- app_jam_uikit/need/extension/CodingUserInfoKey+.swift
+++ app_jam_uikit/need/extension/CodingUserInfoKey+.swift
@@ -5,8 +5,20 @@
 //  Created by Markim Shaw on 2/19/21.
 //
 
+import CoreData
 import Foundation
 
 extension CodingUserInfoKey {
   static let managedObjectContext = CodingUserInfoKey(rawValue: "managedObjectContext")!
+}
+
+extension JSONDecoder {
+  /// Returns a `JSONDecoder` object equipped with the given `NSManagedObjectContext`. Returns a regular `JSONDecoder` object if the provided context is nil.
+  static func decoder(withContext context: NSManagedObjectContext?) -> JSONDecoder {
+    guard let context = context else { return JSONDecoder() }
+    
+    let decoder = JSONDecoder()
+    decoder.userInfo[CodingUserInfoKey.managedObjectContext] = context
+    return decoder
+  }
 }

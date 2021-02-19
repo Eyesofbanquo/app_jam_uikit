27647187231db45107759a853f6cdf6101222f5a
Markim Shaw <ms79723@gmail.com>
2021-02-19 01:54:10 -0600

    ✅⚡️ Add Source type + Coredata extension

---
+++ app_jam_uikit/need/extension/CodingUserInfoKey+.swift
@@ -0,0 +1,12 @@
+//
+//  CodingUserInfoKey+.swift
+//  app_jam_uikit
+//
+//  Created by Markim Shaw on 2/19/21.
+//
+
+import Foundation
+
+extension CodingUserInfoKey {
+  static let managedObjectContext = CodingUserInfoKey(rawValue: "managedObjectContext")!
+}
--- app_jam_uikit/views/GalleryView.swift
+++ app_jam_uikit/views/GalleryView.swift
@@ -22,29 +22,10 @@ class GalleryView: UIView, GalleryViewControllerDelegate {
     return self
   }
   
-  lazy var layout: UICollectionViewCompositionalLayout = {
-    let fraction: CGFloat = 1/3
-    let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(fraction), heightDimension: .fractionalHeight(1))
-    let item = NSCollectionLayoutItem(layoutSize: itemSize)
-    item.contentInsets = .init(top: 4.0, leading: 4.0, bottom: 4.0, trailing: 4.0)
-    
-    let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(fraction))
-    let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-    
-    let section = NSCollectionLayoutSection(group: group)
-    return UICollectionViewCompositionalLayout(section: section)
-  }()
+  lazy var layout: UICollectionViewCompositionalLayout = createCollectionViewLayout()
   
   // MARK: - Lazy Properties -
-  lazy var collectionView: UICollectionView = {
-    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-    collectionView.translatesAutoresizingMaskIntoConstraints = false
-    collectionView.backgroundColor = .systemBackground
-    
-    collectionView.register(GalleryCollectionViewCell.self, forCellWithReuseIdentifier: GalleryCollectionViewCell.reuseIdentifier)
-    
-    return collectionView
-  }()
+  lazy var collectionView: UICollectionView = createCollectionView()
   
   lazy var diffableDataSource = UICollectionViewDiffableDataSource<GalleryViewSection, Colorino>(collectionView: collectionView) { (collectionView, indexPath, item) -> UICollectionViewCell? in
     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GalleryCollectionViewCell.reuseIdentifier, for: indexPath)
@@ -57,13 +38,7 @@ class GalleryView: UIView, GalleryViewControllerDelegate {
   init() {
     super.init(frame: .zero)
     
-    addSubview(collectionView)
-    collectionView.snp.makeConstraints { make in
-      make.leading.equalTo(layoutMarginsGuide.snp.leading)
-      make.trailing.equalTo(layoutMarginsGuide.snp.trailing)
-      make.bottom.equalTo(layoutMarginsGuide.snp.bottom)
-      make.top.equalTo(layoutMarginsGuide.snp.top)
-    }
+    setupCollectionView()
   }
   
   required init?(coder: NSCoder) {
@@ -76,10 +51,49 @@ class GalleryView: UIView, GalleryViewControllerDelegate {
     updateSnapshot(withItems: [.init(color: .black), .init(color: .green), .init(color: .red)])
   }
   
-  func updateSnapshot(withItems items: [Colorino] = []) {
+  
+}
+
+// MARK: - Private API -
+extension GalleryView {
+  
+  private func updateSnapshot(withItems items: [Colorino] = []) {
     var diffableDataSourceSnapshot = NSDiffableDataSourceSnapshot<GalleryViewSection, Colorino>()
     diffableDataSourceSnapshot.appendSections(GalleryViewSection.allCases)
     diffableDataSourceSnapshot.appendItems(items, toSection: .gyazo)
     diffableDataSource.apply(diffableDataSourceSnapshot)
+  }
+  
+  private func setupCollectionView() {
+    addSubview(collectionView)
+    collectionView.snp.makeConstraints { make in
+      make.leading.equalTo(layoutMarginsGuide.snp.leading)
+      make.trailing.equalTo(layoutMarginsGuide.snp.trailing)
+      make.bottom.equalTo(layoutMarginsGuide.snp.bottom)
+      make.top.equalTo(layoutMarginsGuide.snp.top)
+    }
+  }
+  
+  private func createCollectionView() -> UICollectionView {
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+    collectionView.translatesAutoresizingMaskIntoConstraints = false
+    collectionView.backgroundColor = .systemBackground
+    
+    collectionView.register(GalleryCollectionViewCell.self, forCellWithReuseIdentifier: GalleryCollectionViewCell.reuseIdentifier)
+    
+    return collectionView
+  }
+  
+  private func createCollectionViewLayout() -> UICollectionViewCompositionalLayout {
+    let fraction: CGFloat = 1/3
+    let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(fraction), heightDimension: .fractionalHeight(1))
+    let item = NSCollectionLayoutItem(layoutSize: itemSize)
+    item.contentInsets = .init(top: 4.0, leading: 4.0, bottom: 4.0, trailing: 4.0)
+    
+    let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(fraction))
+    let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+    
+    let section = NSCollectionLayoutSection(group: group)
+    return UICollectionViewCompositionalLayout(section: section)
   }
 }
